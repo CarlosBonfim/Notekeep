137470fc0904d682e0d09d49b1b00f5097564b3d
CarlosBonfim <brunobonfim10@hotmail.com>
2023-06-12 16:39:20 -0400

    aterada a estrutura de arquivos js
    O arquivo index.js passará a se chamar client.js e foi criado um novo arquivo index.js.

---
+++ js/client.js
@@ -0,0 +1,52 @@
+const url = "http://localhost:3000/notekeep";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    const notes = document.getElementsByClassName("notes")[0];
+    data.forEach((element) => {
+      const note = document.createElement("div");
+      note.classList.add("note");
+      const id = element._id
+      note.innerHTML = `<div class="noteContent" onclick="selectNote('${id}')" ><h3 class="noteTitle">${element.noteTitle}</h3><p class="nodeText">${element.noteText}</p></div>`;
+      notes.appendChild(note);
+    });
+  })
+  .catch((err) => console.log(`Caiu no catch ${err}`));
+
+const formNote = document.getElementById("noteFormId");
+formNote.addEventListener("submit", (event) => {
+  event.preventDefault();
+  submitFormNote();
+  formNote.reset();
+});
+
+function submitFormNote() {
+  console.log("entrou no submit form note");
+  // const formNote = document.getElementById("noteFormId");
+  const formData = new FormData(formNote);
+  const data = {};
+  for (let [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+  console.log(data);
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.text())
+    .then((text) => console.log(`Resposta do servidor: ${text}`))
+    .then(() => location.reload())
+    .catch((err) =>
+      console.log(
+        `ERRO, NÃO FOI POSSIVEL ENVIAR OS DADOS PARA O SERVIDOR: ${err}`
+      )
+    );
+}
+
+function selectNote(id){
+  console.log(id)
+}
